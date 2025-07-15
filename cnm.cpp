142850150c08265d0b42c1c22c8c8c144271acdd
LKB-hub <18975537758@163.com>
2025-07-15 16:18:59 +0800

    Add files via upload

---
+++ cnm.cpp
@@ -0,0 +1,2 @@
+#include<iostream>
+using namespace std;void fun(){int b=4;while(b){do{for(int c=0;c<1;c++){if(b==1){cout<<"helloworld"<<endl;}}b--;}while(b);}}int main(){int i=1;while(i){do{for(int a=0;a<1;a++){if(a==0){fun();}}i--;}while(i);}return 0;}
